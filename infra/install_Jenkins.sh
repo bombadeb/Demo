ad11dc162c4b1d6b12d75b1fef9ecd8c9681c056
bombadeb <bomba.deb@gmail.com>
2020-11-02 18:47:51 +0530

    Update install_Jenkins.sh

--- infra/install_Jenkins.sh
+++ infra/install_Jenkins.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 sudo apt update
-sudo apt install openjdk-8-jdk
+sudo apt install openjdk-8-jdk -y
 sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
+sudo apt install software-properties-common
+sdo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
